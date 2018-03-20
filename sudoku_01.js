ad7ba61ed18b1554463b93a29d35fce0ea9359cc
abhay <arajpal@beestpeers.com>
2018-03-20 16:49:50 +0530

    sudoku completed

--- sudoku_01.js
+++ sudoku_01.js
@@ -3,42 +3,41 @@ $(document).ready(function(){
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   var RandomArray = []; 
-  var easyArray = [ 
-    [8,2,7,1,5,4,3,9,6],
-    [9,6,5,3,2,7,1,4,8],
-    [3,4,1,6,8,9,7,5,2], 
-    [5,9,3,4,6,8,2,7,1], 
-    [4,7,2,5,1,3,6,8,9], 
-    [6,1,8,9,7,2,4,3,5],
-    [7,8,6,2,3,5,9,1,4],
-    [1,5,4,7,9,6,8,2,3],
-    [2,3,9,8,4,1,5,6,7] 
+  var mainArray = [ 
+    [5,3,4,6,7,8,9,1,2],
+    [6,7,2,1,9,5,3,4,8],
+    [1,9,8,3,4,2,5,6,7], 
+    [8,5,9,7,6,1,4,2,3], 
+    [4,2,6,8,5,3,7,9,1], 
+    [7,1,3,9,2,4,8,5,6],
+    [9,6,1,5,3,7,2,8,4],
+    [2,8,7,4,1,9,6,3,5],
+    [3,4,5,2,8,6,1,7,9] 
   ]
-  var RandomArraySolution = [];
   var shuffle = function (){
-    var switch1 = getRandomInt(1,9);
-    var switch2 = getRandomInt(1,9);
-    while (switch1 ===switch2){
-      switch1 = getRandomInt(1,9);
+    var num1 = getRandomInt(1,9);
+    var num2 = getRandomInt(1,9);
+    while (num1 ===num2){
+      num1 = getRandomInt(1,9);
     } 
     for (i=0; i <9; i++){
       for (j=0; j<9; j++){
-        if (easyArray[i][j] === switch1){
-         easyArray[i][j] = switch2;
+        if (mainArray[i][j] === num1){
+         mainArray[i][j] = num2;
         } 
-        else if (easyArray[i][j] === switch2){
-          easyArray[i][j] = switch1;
+        else if (mainArray[i][j] === num2){
+          mainArray[i][j] = num1;
         }
       }  
     }
   }
-  $("#Puzzle_Gen").on('click', function (){ 
+  $("#random_num").on('click', function (){ 
     shuffle();
-    RandomArray = $.extend(true,[], easyArray);
+    RandomArray = $.extend(true,[], mainArray);
     for (i=0; i <9; i++){
-      var getRandomNumHide = getRandomInt(1,5);
+      var getRandomNumHidden = getRandomInt(1,5);
       var choices = [0,1,2,3,4,5,6,7,8];
-      for (j=0; j < getRandomNumHide; j++){
+      for (j=0; j < getRandomNumHidden; j++){
         var index = getRandomInt(0, choices.length - 1); 
         var position = choices.splice(index, 1)[0];
         RandomArray[i][position]='';
@@ -46,13 +45,13 @@ $(document).ready(function(){
     }
     for (i=0; i<9; i++){
       for (j=0; j<9; j++){
-        var divA = $("#box" + (i + 1) + (j + 1));
-        var rAndC = $(divA);
-        rAndC.text(RandomArray[i][j]);
-        if(rAndC.text()===''){
-        rAndC.attr("contentEditable", 'true');
+        var printValue = $("#box" + (i + 1) + (j + 1));
+        var printValue1 = $(printValue);
+        printValue1.text(RandomArray[i][j]);
+        if(printValue1.text()===''){
+        printValue1.attr("contentEditable", 'true');
         }else{
-          rAndC.attr("contentEditable", 'false');
+          printValue1.attr("contentEditable", 'false');
         }
       } 
     }
@@ -61,14 +60,14 @@ $(document).ready(function(){
     var counter = 0;
     for (i=0; i<9; i++){
       for(j=0; j<9; j++){
-        var divC = $("#bx" + (i + 1) + (j+ 1));
-        RandomArray[i][j] = $(divC).val();
-        var divD = $("#box" + (i + 1) + (j+ 1));
-        if (easyArray[i][j] != RandomArray[i][j]) {
-          $(divD).css({"background-color": "#FECBAE"});
+        var checkValue = $("#box" + (i + 1) + (j+ 1));
+        RandomArray[i][j] = $(checkValue).text();
+        var checkValue1 = $("#box" + (i + 1) + (j+ 1));
+        if (mainArray[i][j] != RandomArray[i][j]) {
+          $(checkValue1).css({"background-color": "#708238"});
         counter++;
         }else {
-          $(divD).css({"background-color": ""})
+          $(checkValue1).css({"background-color": ""})
         }
       }
     }
@@ -76,4 +75,7 @@ $(document).ready(function(){
       alert("you win!");
      }
   });
+  $("#clear").on('click',function(){
+    location.href ="sudoku_01.html";
+  })
 });
